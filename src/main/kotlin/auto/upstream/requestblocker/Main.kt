3a06617a9c2a008b12bb4ccc39c46db16f66495e
Eyal Ringort <ringort@gmail.com>
2020-10-22 12:48:09 +0300

    Added RequestBlocker logic

--- src/main/kotlin/auto/upstream/requestblocker/Main.kt
+++ src/main/kotlin/auto/upstream/requestblocker/Main.kt
@@ -1,5 +1,26 @@
 package auto.upstream.requestblocker
 
-fun main(args : Array<String>) {
-    println("Hello, World!")
+import auto.upstream.requestblocker.block.RequestBlocker
+
+fun main(args: Array<String>) {
+
+//    // For testing
+//    val requestBlocker1 = RequestBlocker(listOf("192.170.1.0/28", "192.170.0.0/10"))
+//    val ip1 = "192.170.1.1" // Not in range
+//    val ip2 = "192.170.5.5" // Not in range
+//
+//    prettyPrint(ip1, requestBlocker1)
+//    prettyPrint(ip2, requestBlocker1)
+
+    // For testing
+    val requestBlocker2 = RequestBlocker(listOf("192.170.1.0/28"))
+    val ip1 = "192.170.1.1" // In range
+    val ip2 = "192.170.5.5" // Not in range
+
+    prettyPrint(ip1, requestBlocker2)
+    prettyPrint(ip2, requestBlocker2)
+}
+
+private fun prettyPrint(ip2: String, requestBlocker: RequestBlocker) {
+    println("IP: " + ip2 + " is " + (if (requestBlocker.isAllowed(ip2)) "" else "not ") + "allowed")
 }
--- src/main/kotlin/auto/upstream/requestblocker/block/RequestBlocker.kt
+++ src/main/kotlin/auto/upstream/requestblocker/block/RequestBlocker.kt
@@ -1,8 +1,18 @@
 package auto.upstream.requestblocker.block
 
-class RequestBlocker {
+import org.apache.commons.net.util.SubnetUtils
+
+class RequestBlocker(blockedcCidrs: Collection<String>) {
+
+    private var cidars = mutableListOf<SubnetUtils>()
+
+    init {
+        for (blockedCidr in blockedcCidrs) {
+            this.cidars.add(SubnetUtils(blockedCidr))
+        }
+    }
 
     fun isAllowed(incomingIp: String): Boolean {
-
+        return !cidars.stream().filter { it.info.isInRange(incomingIp) }.findAny().isPresent
     }
 }
